4410b55b1ca9411ab33bdd238a43c859dea0c91d
Joseph Norman <jnormantransactions@gmail.com>
2020-09-20 14:24:11 -0500

    Update get_urls.js

--- get_urls.js
+++ get_urls.js
@@ -1,3 +1,13 @@
+// copy/paste this to automatically fetch all of the recommended links
 setInterval(function() {
     document.querySelectorAll('span[data-translation-id="recipe-archive.pagination.more-recipes"')[0].click();
 }, 5000);
+
+// once complete, copy/paste this to get the entire list of urls
+const links = document.querySelectorAll('a[href^="/recipes/"].dsbi');
+const urls = [];
+for(const link of links)
+{
+     urls.push(link.href);   
+}
+console.log(urls);
