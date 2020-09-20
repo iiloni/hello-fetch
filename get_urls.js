2e925851f9723fc5fa4e255ad47d9fb9ec09a523
Joseph Norman <jnormantransactions@gmail.com>
2020-09-20 15:11:57 -0500

    Update get_urls.js

--- get_urls.js
+++ get_urls.js
@@ -3,25 +3,14 @@ const body = document.body;
 const button = document.querySelectorAll('span[data-translation-id="recipe-archive.pagination.more-recipes"')[0];
 
 var last_scroll = 0;
-var urls = [];
 
 const interval_id = setInterval(function() {
     if(last_scroll == body.scrollHeight) // if nothing added to the page
     {
         clearInterval(interval_id); // stops the button clicking
-
-        // a list of links will be queried
-        const links = document.querySelectorAll('a[href^="/recipes/"].dsbi');
         
-        // and those links will be parsed for the hrefs
-        for(const link of links)
-        {
-             urls.push(link.href);   
-        }
-        
-        // reports completion and full list of urls
+        // reports completion
         console.log('Completed!');
-        console.log(urls);
     }
     else // there are still more items to see
     {
@@ -29,4 +18,4 @@ const interval_id = setInterval(function() {
         button.click(); // clicks the button
         console.log('Clicking...');
     }   
-}, 4000);
+}, 2500);
